ac5ea0cf9c5a109a649ae35b864822b7821e8ab1
Marcus Silva <marcusluis.mail@gmail.com>
2024-01-27 20:52:46 -0300

    adding a new colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,11 @@
 -- Colorscheme
 return {
-    "rebelot/kanagawa.nvim",
+    "Mofiqul/adwaita.nvim",
+    lazy = false,
+    priority = 1000,
+
     config = function()
-        vim.cmd("colorscheme kanagawa-wave")
-    end,
+        vim.g.adwaita_darker = true
+        vim.cmd('colorscheme adwaita')
+    end
 }
