ffc2ed710dfc6d65c5fb4ebf4929e0882ee93cf5
Marcus Silva <marcusluis.mail@gmail.com>
2024-02-09 21:05:37 -0300

    adding new arrows icons

--- nvim/lua/plugins/nv-tree.lua
+++ nvim/lua/plugins/nv-tree.lua
@@ -23,8 +23,8 @@ return {
                 icons = {
                     glyphs = {
                         folder = {
-                            arrow_closed = "",
-                            arrow_open = "",
+                            arrow_closed = "",
+                            arrow_open = "",
                         },
                     },
                 },
