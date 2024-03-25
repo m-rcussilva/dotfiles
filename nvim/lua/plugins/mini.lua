3eaacd9a5f501fa70a347f56d68fd1de1fb73bd2
m-rcussilva <marcusluis.mail@gmail.com>
2024-03-25 10:40:49 -0300

    instruction how to add double quotes

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -10,7 +10,7 @@ return {
         require('mini.ai').setup { n_lines = 500 }
 
         -- Add/delete/replace surroundings (brackets, quotes, etc.)
-        --
+        -- - VISUAL MODE - sa "" - to add double quotes
         -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
         -- - sd'   - [S]urround [D]elete [']quotes
         -- - sr)'  - [S]urround [R]eplace [)] [']
